fb9653de5940c83b143b1087b2f91de067b7a4b7
Technokarts@123 <priyanka.nevase@technokarts.com>
2020-07-02 15:29:04 +0530

    Initial commit

---
+++ app/src/main/java/com/example/paypaldemo/PayPalConfig.java
@@ -0,0 +1,5 @@
+package com.example.paypaldemo;
+
+public class PayPalConfig {
+    public static final String PAYPAL_CLIENT_ID = "AQsNde_iz_YkNMFwqrAUCncEryD4GdNXlJDtwwx5xR861bnMEsiroETNvMo5ZtkrnJrbQ2k1Z5wDJ7fH";
+}
